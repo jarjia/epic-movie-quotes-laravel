d2df6038e37234796d26c92ec70159af8c36ac84
jarji <100996174+jarjia@users.noreply.github.com>
2023-05-24 15:09:51 +0400

    feat: add custom mail class

---
+++ app/Mail/VerificationEmail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerificationEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public $user, public $token)
+    {
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(address: 'epic@moviequotes.com', name: 'Epic Movie Quotes')
+            ->subject(subject: 'Email Verification')
+            ->view('mails.verify');
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
