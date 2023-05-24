5321f31ef5d41ccd398283ce0bd0811605bba171
jarji <100996174+jarjia@users.noreply.github.com>
2023-05-24 19:57:50 +0400

    feat: add mail template for password recover

---
+++ app/Mail/PasswordRecoverMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordRecoverMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public $user, public $token)
+    {
+    }
+
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(address: 'epic@moviequotes.com', name: 'Epic Movie Quotes')
+            ->subject(subject: 'Recover Password')
+            ->view('mails.recover');
+    }
+}
---
+++ resources/views/mails/recover.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+    </head>
+    <body style="font-family: 'Roboto'; width:full; height:full; ">
+        <div style="width: 100%; height: 100%; background-color: #181624; color:white; padding-top: 2rem; padding-bottom: 2rem;">
+            <div style="margin: auto; width: 10%; font-weight: bold;">
+                <x-svg-quote />              
+            </div>
+            <div style="margin: auto; margin-top:2%; width: 18%; height:10%; color:#DDCCAA;">
+                MOVIE QUOTES
+            </div>
+            <div style="margin-left: 3%; width: 48%; height:10%">
+                <span style="color: white;">Hola again {{$user->name}}!</span>
+            </div>
+            <div style="margin-left: 3%; width: 80%; height:10%;">
+                <p style="color: white;">
+                    You can recover your password by clicking this button:
+                </p>
+            </div>
+            <div style="margin-left: 3%;">
+                <div>
+                    <a style="background-color: #E31221; border-radius: 4px; padding: 10px 30px; text-decoration:none; color:white;" 
+                    href="{{config('app.front_url').'/?recover_token='.$token.'&email='.$user->email}}">
+                    Recover password</a>
+                </div>
+            </div>
+            <div style="margin-left: 3%; width: 80%;">
+                <p style="color: white;">
+                    If clicking doesn't work, you can try copying and pasting it to your browser:
+                </p>
+            </div>
+            <div style="margin-left: 3%; width: 80%; height: 20%;">
+                <p style="word-wrap: break-word; color:#DDCCAA;">
+                    {{config('app.front_url').'/?recover_token='.$token.'&email='.$user->email}}
+                </p>
+            </div>
+            <div style="margin-left:3%; width: 80%;">
+                <p style="color: white;">
+                    If you have any problems, please contact us: <span style="color: white;">support@moviequotes.ge</span>              
+                </p>
+            </div>
+            <div style="margin-left:3%; width: 80%;">
+                <p style="color: white;">
+                    MovieQuotes Crew                
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
