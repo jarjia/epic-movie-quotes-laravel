23a91b3cd590b07b98ad8a43d88bc8c2b6a4fe91
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-08 14:23:21 +0400

    feat: write seeder for setting up genres pre-defined array

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Genre;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $defaultGenres = [
+            ['en' => 'Animation', 'ka' => 'ანიმაცია'],
+            ['en' => 'Action', 'ka' => 'ექშენი'],
+            ['en' => 'Romance', 'ka' => 'რომანტიკა'],
+            ['en' => 'Horror', 'ka' => 'ჰორორი'],
+            ['en' => 'Drama', 'ka' => 'დრამა'],
+            ['en' => 'Thriller', 'ka' => 'თრილერი'],
+            ['en' => 'Comedy', 'ka' => 'კომედია'],
+            ['en' => 'Parody', 'ka' => 'პაროდია'],
+            ['en' => 'Fantasy', 'ka' => 'ფენტეზი'],
+            ['en' => 'Historical', 'ka' => 'ისტორიული'],
+            ['en' => 'Western', 'ka' => 'ვესტერნი'],
+            ['en' => 'musical', 'ka' => 'მიუზიკლი'],
+            ['en' => 'Criminal', 'ka' => 'კრიმინალური'],
+            ['en' => 'Documentary', 'ka' => 'დოკუმენტური'],
+            ['en' => 'Adventure', 'ka' => 'სათავგადასავლო'],
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        usort($defaultGenres, function ($a, $b) {
+            $locale = app()->getLocale();
+            return strcmp($a[$locale], $b[$locale]);
+        });
+
+        foreach ($defaultGenres as $genreName) {
+            Genre::create(['genre' => $genreName]);
+        }
     }
 }
