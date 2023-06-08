3dc844adfc119768c5b785da628fb3bf3f589fab
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-08 18:01:47 +0400

    refactor: move genres array in config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,23 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $defaultGenres = [
-            ['en' => 'Animation', 'ka' => 'ანიმაცია'],
-            ['en' => 'Action', 'ka' => 'ექშენი'],
-            ['en' => 'Romance', 'ka' => 'რომანტიკა'],
-            ['en' => 'Horror', 'ka' => 'ჰორორი'],
-            ['en' => 'Drama', 'ka' => 'დრამა'],
-            ['en' => 'Thriller', 'ka' => 'თრილერი'],
-            ['en' => 'Comedy', 'ka' => 'კომედია'],
-            ['en' => 'Parody', 'ka' => 'პაროდია'],
-            ['en' => 'Fantasy', 'ka' => 'ფენტეზი'],
-            ['en' => 'Historical', 'ka' => 'ისტორიული'],
-            ['en' => 'Western', 'ka' => 'ვესტერნი'],
-            ['en' => 'musical', 'ka' => 'მიუზიკლი'],
-            ['en' => 'Criminal', 'ka' => 'კრიმინალური'],
-            ['en' => 'Documentary', 'ka' => 'დოკუმენტური'],
-            ['en' => 'Adventure', 'ka' => 'სათავგადასავლო'],
-        ];
+        $defaultGenres = config('epicmoviequotes.genres');
 
         usort($defaultGenres, function ($a, $b) {
             $locale = app()->getLocale();
