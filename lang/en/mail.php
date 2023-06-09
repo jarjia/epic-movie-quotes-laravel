bbfcd2cede247fe2f09b603bd8f962dc518361bc
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-09 20:59:05 +0400

    feat: add language folder

---
+++ lang/en/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'hello' => 'Hola',
+    'verify_h1' => 'Thanks for joining Movie quotes! We really appreciate it. Please click the button below to verify your account:',
+    'verify_button' => 'Verify account',
+    'verify_h2' => "If clicking doesn't work, you can try copying and pasting it to your browser:",
+    'verify_h3' => 'If you have any problems, please contact us:',
+    'recover_hello' => 'Hola again',
+    'recover_button' => 'Recover password',
+    'recover_h1' => 'You can recover your password by clicking this button:'
+];
