03ec302a3efbd28083767f7e745328c05b6ee192
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-14 18:36:22 +0400

    feat: create request for comment store

---
+++ app/Http/Requests/CommentRequests/PostCommentRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\CommentRequests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostCommentRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'comment' => 'required',
+            'quote_id' => 'required',
+        ];
+    }
+}
