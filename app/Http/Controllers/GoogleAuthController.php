3cd1ba3afcd0f4214269fd365cdd850d0a23ddeb
jarji <100996174+jarjia@users.noreply.github.com>
2023-09-10 22:54:40 +0400

    feat: dont let google change avatar and name on user

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -43,10 +43,7 @@ class GoogleAuthController extends Controller
             $user->remember_token = Str::random(60);
             $user->save();
         } else {
-            $user->name = $googleUser->name;
             $user->google_id = $googleUser->getId();
-            $user->thumbnail = 'images/' . $profile;
-            $user->email_verified_at = now();
             $user->remember_token = Str::random(60);
             $user->save();
         }
